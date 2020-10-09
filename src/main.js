5c3c2b785c1fc4061b19b390054f6b68d29e22f2
mirsella <mirsella@protonmail.com>
2020-10-09 14:21:21 +0200

    gacp ven. 09 oct. 2020 14:21:21 CEST

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import "@/assets/tailwind.css";
 import Ads from 'vue-google-adsense'
 
-App.use(require('vue-script2'))
-// App.use(Ads.Adsense)
-App.use(Ads.AutoAdsense, { adClient: 'ca-pub-5245653360027652' })
+const app = createApp(App)
 
-createApp(App).mount('#app')
+app.use(require('vue-script2'))
+app.use(Ads.AutoAdsense, { adClient: 'ca-pub-5245653360027652' })
+// app.use(Ads.Adsense)
+
+app.mount('#app')
