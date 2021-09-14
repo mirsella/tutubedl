1dd723be35187f665352ab1372575d24db42ea9d
mirsella <mirsella@protonmail.com>
2021-09-14 18:54:01 +0200

    gcd Tue Sep 14 06:54:01 PM CEST 2021

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const fs = require('fs-extra');
+const morgan = require('morgan');
+const helmet = require('helmet')
+const cors = require('cors')
+require('dotenv').config();
+const app = express();
+app.enable('trust proxy');
+const ytpl = require('ytpl');
+const ytdlwrap = require('youtube-dl-wrap');
+ytdlwrap.downloadFromGithub();
+const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+const ytdl = new ytdlwrap();
+
+app.use(helmet())
+app.use(cors())
+app.use(morgan('common'));
+app.use(express.json());
+
+fs.emptyDirSync('downloaded')
+let filecount = 0
+
+async function dl(url, format, filename) {
+  // options : video / audio
+  const arguments = {
+    audio: [url, "-x", "--no-continue", "--audio-quality", "0", "--audio-format", "mp3", "-f", "bestaudio", "-o", filename, "--ffmpeg-location", ffmpeg.path],
+    video: [url, "-f", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4", "-o", filename, "--ffmpeg-location", ffmpeg.path]
+  }
+  return new Promise(resolve => {
+    ytdl.exec(arguments[format])
+      .on("progress", (progress) => console.log(progress.percent, progress.totalSize, progress.currentSpeed, progress.eta))
+      .on("youtubeDlEvent", (eventType, eventData) => console.log(eventType, eventData))
+      .on("error", (error) => console.error(error))
+      .once("close", () => {
+        console.log("all done")
+        resolve()
+      })
+  })
+}
+
+app.use(async (req,res,next) => {
+  const body = req.body
+  if (body.url) {
+    next()
+  } else {
+    res.statusMessage = "didn't include url"
+    res.sendStatus(400)
+  }
+})
+
+app.all('/audio', async (req,res) =>{
+  url = req.body.url || 'yjMvLJZ9j0I'
+  const filename = `downloaded/${filecount}.%(ext)s`
+  const filenamefinal = `downloaded/${filecount}.mp3`
+  filecount += 1
+  await dl(url, 'audio', filename)
+  res.download(filenamefinal)
+  fs.remove(filenamefinal)
+});
+
+app.all('/video', async (req,res) =>{
+  url = req.body.url || 'yjMvLJZ9j0I'
+  const filename = `downloaded/${filecount}.mp4`
+  filecount += 1
+  await dl(url, 'video', filename)
+  res.download(filename)
+  fs.remove(filename)
+});
+
+app.all('/getinfo', async (req,res) =>{
+  res.json(await ytdl.getVideoInfo( req.body.url || 'yjMvLJZ9j0I' ))
+});
+
+app.all('/playlist', async (req,res) =>{
+  url = req.body.url || 'PLnYA0n5BTNscRlnFBkNGJrCyKdOqGtID9' 
+  if (await ytpl.validateID(url)) {
+    res.json(
+      await ytpl(url)
+      .then(data => {
+        return data
+      })
+      .catch(e => {
+        res.statusMessage(e)
+        res.sendStatus(500)
+      })
+    )
+  } else {
+      res.statusMessage = 'invalid url'
+      res.sendStatus(400)
+    }
+});
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => console.log(`app listening on ${PORT}`))
+
---
+++ src.tutubedl/App.vue
@@ -0,0 +1,155 @@
+<template>
+  <main class="flex flex-col min-h-screen font-bold text-white bg-black" :class="[ bgstyles[0] ? bgstyles[1]: '' ]">
+    <h1 class="m-5 text-xl text-center text-transparent md:text-5xl bg-clip-text bg-gradient-to-r from-pink-600 via-purple-600 to-purple-900">Yet Another Youtube Downloader</h1>
+    <form @submit.prevent="list()" name="form" class="inline-flex my-2">
+      <a @mouseover="gitover=true" @mouseleave="gitover=false" href="https://github.com/mirsella/tutubedl" target="_blank" class="m-auto mx-1">
+        <img v-if="!gitover" src="./assets/github.png" alt="github" class="w-12">
+        <img v-if="gitover" src="./assets/github-face.png" alt="github" class="w-12">
+      </a>
+      <input type="text" v-model="url" placeholder="youtube url" class="w-full h-10 p-2 text-base leading-normal rounded-l-lg bg-gradient-to-r from-pink-600 to-purple-600 md:text-lg placeholder-current focus:outline-none">
+      <button class="inline-flex justify-center ml-1 mr-2 focus:outline-none">
+        <span class="w-24 h-10 px-4 py-2 rounded-r-lg md:text-lg md:w-48 bg-gradient-to-r from-purple-600 to-purple-900 hover:from-purple-700">
+          <svg v-if="loading.get" class="inline-flex w-5 h-4 mb-1 mr-3 text-white animate-spin" fill="none" viewBox="0 0 24 24">
+            <path class="bg-purple-600 opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+          <span :class="{ hidden: loading.get }" class="md:inline-block">search</span>
+        </span>
+      </button>
+    </form>
+    <h1 v-if="err" class="m-10 text-red-700 place-self-center">{{err}}</h1>
+    <div v-if="videos.length > 1" class="m-5 w-min place-self-center">
+      <div class="p-1 mb-1 text-center rounded-t-lg bg-gradient-to-r from-pink-500 via-pink-600 to-purple-700">
+        download all :
+      </div>
+      <div class="w-auto inline-flex">
+        <button @click="downloadall('audio')" class="px-8 py-2 mr-1 text-white bg-purple-700 rounded-bl-lg bg-gradient-to-r from-pink-500 to-pink-600 focus:outline-none place-self-center">
+          audio
+        </button>
+        <button @click="downloadall('video')" class="px-8 py-2 text-white bg-purple-700 rounded-br-lg bg-gradient-to-r from-pink-600 to-purple-700 focus:outline-none place-self-center">
+          video
+        </button>
+      </div>
+      <svg v-if="loading.all" class="w-5 mx-auto mt-5 animate-spin place-self-center" fill="none" viewBox="0 0 24 24">
+        <path class="bg-purple-600 opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+    </div>
+    <div :key="video.title" v-for="(video, index) in videos">
+      <div class="items-center block m-10 lg:flex">
+        <iframe class="m-2 md:m-5 md:w-4/12 w-42" :src="'https://youtube.com/embed/'+video.id" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div>
+          <div class="m-3 text-center md:text-left">
+            <button @click="onedownload(video, 'audio', index)" class="px-4 py-2 mx-px text-center text-white bg-purple-700 rounded-l-lg bg-gradient-to-r from-pink-500 to-pink-600 focus:outline-none hover:bg-purple-800" target="_blank">
+              audio
+            </button>
+            <button @click="onedownload(video, 'video', index)" class="px-4 py-2 mx-px text-center text-white bg-purple-700 rounded-r-lg bg-gradient-to-r from-pink-600 to-purple-700 focus:outline-none hover:bg-purple-800" target="_blank">
+              video
+            </button>
+            <svg v-if="loading.single[index]" class="inline-block h-4 ml-2 text-white animate-spin" fill="none" viewBox="0 0 24 24">
+              <path class="bg-purple-600 opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+          </div>
+          <span class="m-2 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-pink-600 to-purple-700">{{video.title}}</span>
+          <span class="block m-2 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-pink-600 to-purple-700">{{video.duration}}</span>
+        </div>
+      </div>
+    </div>
+    <button :class="[ !bgstyles[2] ? bgstyles[3]: '' ]" class="w-48 ml-1 text-sm text-transparent focus:outline-none bg-clip-text bg-gradient-to-r from-orange-400 via-red-500 to-pink-500" @click="bgstyles[0] = !bgstyles[0]">toggle mango background :)</button>
+  </main>
+</template>
+
+<script>
+import download from 'downloadjs'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      api: import.meta.env.VITE_API || '',
+      url: '',
+      err: '',
+      gitover: false,
+      bgstyles: [ false, 'bg-gradient-to-r from-orange-400 via-red-500 to-pink-500', false, 'fixed bottom-0' ],
+      loading: {
+        all: false,
+        get: false,
+        single: []
+      },
+      videos: [],
+    }
+  },
+  mounted() {
+    // ping heroku server to pre wake it up
+    if (this.api.includes('herokuapp')) {
+      console.log('ping', this.api)
+      fetch(this.api)
+        .then(() => console.log('finished ping', this.api))
+      // .catch(err => this.err = "could not reach server " + err)
+    }
+  },
+  methods: {
+    async list() { 
+      this.loading.get = true
+      this.err = ''
+      await fetch(this.api+'/check', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          url: this.url
+        })
+      })
+      // .then(res => res.json())
+        .then(res => {
+          if(res.ok) {
+            res.json().then(body => {
+              this.videos = body
+              setTimeout(() => { 
+                this.bgstyles[2] = document.querySelector('main').offsetHeight > screen.height ? true : false
+              }, 0)
+            })
+            let index
+            for(index in this.videos) {
+              this.loading.single[index] = false
+            }
+          } else {
+            this.err = res.statusText;
+          }
+        })
+        .catch(err => this.err = err)
+      this.loading.get = false
+    },
+    async downloadall(type) {
+      this.loading.all = true
+      let promises = []
+      let video
+      for (video of this.videos) {
+        promises.push(this.download(video, type))
+      }
+      Promise.all(promises)
+        .then(() => this.loading.all = false)
+    },
+    async onedownload(video, type, index) {
+      this.loading.single[index] = true
+      await this.download(video, type)
+      this.loading.single[index] = false
+    },
+    async download(video, type) {
+      await fetch(this.api+`/${type}?id=${video.id}`, {
+        method: 'GET'
+      })
+        .then(resp => resp.blob())
+        .then(blob => {
+          if (type == "audio") {
+            download(blob, video.title.replace(/[^\x20-\x7E]/g, "")+'.mp3');
+          } else {
+            download(blob, video.title.replace(/[^\x20-\x7E]/g, "")+'.mp4');
+          }
+        })
+        .catch(err => {
+          this.err = err;
+        })
+    }
+  }
+}
+</script>
