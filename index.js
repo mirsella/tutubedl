5077150cb3cc19b47bc3e5558416fd3588856669
mirsella <mirsella@protonmail.com>
2021-10-05 00:09:17 +0200

    Tue Oct  5 12:09:17 AM CEST 2021

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const ffmpeg = require('@ffmpeg-installer/ffmpeg');
 const ytdl = new ytdlwrap();
 
 app.use(helmet())
-app.use(cors())
+app.use(cors({ origin: true, credentials: true }));
+// app.use(cors())
 app.use(morgan('common'));
 app.use(express.json());
 
